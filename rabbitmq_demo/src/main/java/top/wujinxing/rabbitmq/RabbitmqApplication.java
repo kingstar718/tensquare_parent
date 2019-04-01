a019caf55e062e112f38d1b853b21fc3d7761fde
kingstar718 <936016045@qq.com>
2019-04-01 22:22:43 +0800

    rabbitmq demo

---
+++ rabbitmq_demo/src/main/java/top/wujinxing/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,17 @@
+package top.wujinxing.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wujinxing
+ * @date: 2019/4/1 17:41
+ * @description:
+ */
+@SpringBootApplication
+public class RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class);
+    }
+}
