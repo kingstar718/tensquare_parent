b29e3a8d7b78822b7651bd0d00d5cdc0cd6293c7
kingstar718 <936016045@qq.com>
2019-04-01 23:45:47 +0800

    user

---
+++ tensquare_user/src/main/java/top/wujinxing/user/pojo/User.java
@@ -0,0 +1,36 @@
+package top.wujinxing.user.pojo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * @author: wujinxing
+ * @date: 2019/4/1 23:02
+ * @description:
+ */
+@Data
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    @Id
+    private String id;//ID
+    private String password;//密码
+    private String mobile;//手机号码
+    private String nickname;//昵称
+    private String sex;//性别
+    private java.util.Date birthday;//出生年月日
+    private String avatar;//头像
+    private String email;//E-Mail
+    private java.util.Date regdate;//注册日期
+    private java.util.Date updatedate;//修改日期
+    private java.util.Date lastdate;//最后登陆日期
+    private Long online;//在线时长（分钟）
+    private String interest;//兴趣
+    private String personality;//个性
+    private Integer fanscount;//粉丝数
+    private Integer followcount;//关注数
+}
